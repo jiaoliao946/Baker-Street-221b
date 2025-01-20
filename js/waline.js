e6d3170df259007523b8de70156a403024ac778d
jiaoliao946 <1686950986@qq.com>
2025-01-20 16:03:31 +0800

    Fix Waline

--- js/waline.js
+++ js/waline.js
@@ -2,7 +2,7 @@ import { init } from "https://unpkg.com/@waline/client@v3/dist/waline.js";
 window.onload = function waline() {
     init({
         el: "#waline",
-        serverURL: "http://localhost:3000",
+        serverURL: "https://baker-street-221b.pages.dev",
         locale: {
             level0: "石器时代",
             level1: "来硬的",
